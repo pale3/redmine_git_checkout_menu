5c78ef44387422df6c56e967d17147859a3fbd59
Marko Rakamaric <marko.rakamaric@helmholz.de>
2024-12-12 12:15:48 +0100

    remove unneeded logs

--- app/controllers/redmine_git_checkout_menu/repository_controller.rb
+++ app/controllers/redmine_git_checkout_menu/repository_controller.rb
@@ -3,7 +3,6 @@ module RedmineGitCheckoutMenu
     before_action :find_project
 
     def download
-      Rails.logger.info "RepositoryDownloadsController: Download action triggered"
       repository = @project.repository
       if repository && repository.url.present?
         repo_path = params[:repo_path]
@@ -13,15 +12,10 @@ module RedmineGitCheckoutMenu
         archive_name = "#{File.basename(repo_path)}-#{branch}.zip"
         archive_path = Rails.root.join('tmp', archive_name).to_s
 
-        Rails.logger.info "PATH command: #{archive_path}"
         # Use git to create an archive of the specified branch
         command = "git --git-dir=#{repo_full_path} archive --format=zip --output=#{archive_path.shellescape} #{branch.shellescape}"
-        Rails.logger.info "Executing command: #{command}"
-        Rails.logger.info "DATAAAA: #{repository.inspect}"
 
-        if system(command)
-          Rails.logger.info "Archive command executed successfully"
-        else
+        if !system(command)
           Rails.logger.error "Archive command failed with status #{$?.exitstatus}"
           render plain: 'Failed to create archive', status: :internal_server_error
           return
@@ -29,13 +23,7 @@ module RedmineGitCheckoutMenu
 
         if File.exist?(archive_path)
           file_size = File.size(archive_path)
-          Rails.logger.info "Archive file exists at #{archive_path}, size: #{file_size} bytes"
-
-          # Log the first few bytes for debugging
-          File.open(archive_path, 'rb') do |file|
-            first_bytes = file.read(20)
-            Rails.logger.info "First 20 bytes of the file: #{first_bytes.inspect}"
-          end
+          Rails.logger.info "Archive command executed successfully: #{archive_path}, size: #{file_size} bytes"
 
           # Ensure proper permissions
           FileUtils.chmod(0o644, archive_path)

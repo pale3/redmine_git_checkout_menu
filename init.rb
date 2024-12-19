21f3eb50fe97d94fbb9aa6a02f1306fb02f239a3
Marko Rakamaric <marko.rakamaric@helmholz.de>
2024-12-19 12:34:06 +0100

    init: add url

--- init.rb
+++ init.rb
@@ -1,7 +1,8 @@
 Redmine::Plugin.register :redmine_git_checkout_menu do
   name 'Redmine Simple Repository Plugin'
   author 'Marko Rakamaric'
-  description 'This plugin gets the repository URL when on the repository tab'
+  description 'This plugin gets/show the repository URL when on the repository tab'
+  url "https://github.com/pale3/redmine_git_checkout_menu"
   version '1.1.0'
   requires_redmine version_or_higher: '6.0.0'
 end
